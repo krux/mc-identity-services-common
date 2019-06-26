0fac0dee2c2b3361385cc24f92f3d71fa20c0bb8
Tej Vepa <venkata.vepa@salesforce.com>
2019-06-25 17:27:29 -0700

    changed versioning

--- src/main/java/com/salesforce/mc/identity/common/schema/CBKDataPathRequest.java
+++ src/main/java/com/salesforce/mc/identity/common/schema/CBKDataPathRequest.java
@@ -2,14 +2,14 @@ package com.salesforce.mc.identity.common.schema;
 
 import java.util.List;
 
-public class CBKFilePathRequest {
+public class CBKDataPathRequest {
 
 	private final String org;
 	private final String basePath;
 	private final List<String> bridgeKeys;
 	private final List<String> sourceTypes;
 
-	public CBKFilePathRequest(final String org, final String basePath, final List<String> bridgeKeys,
+	public CBKDataPathRequest(final String org, final String basePath, final List<String> bridgeKeys,
 			final List<String> sourceTypes) {
 		this.org = org;
 		this.basePath = basePath;
