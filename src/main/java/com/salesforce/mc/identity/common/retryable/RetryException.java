a1cdb056753217a81f958928e1e6c5b160b02edf
Tej Vepa <venkata.vepa@salesforce.com>
2019-08-22 15:10:27 -0700

    Fix unwanted line

--- src/main/java/com/salesforce/mc/identity/common/retryable/RetryException.java
+++ src/main/java/com/salesforce/mc/identity/common/retryable/RetryException.java
@@ -2,7 +2,6 @@ package com.salesforce.mc.identity.common.retryable;
 
 public class RetryException extends Exception {
 
-	private static final long serialVersionUID = -7407775622713419770L;
 	public final Exception e;
 
 	public RetryException(final Exception e) {
